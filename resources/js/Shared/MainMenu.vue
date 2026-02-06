d4028e4b7f1eecccc53bd82963cbb83048944a46
Jocelyn-AW <jocelyn@awsoftware.mx>
2026-02-06 17:25:31 -0600

    first commit

---
+++ resources/js/Shared/MainMenu.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { Link, usePage } from '@inertiajs/vue3'
+import icon from '@/Shared/Icon.vue'
+
+const isUrl = (url) => {
+    let page = usePage();
+    const currentUrl = page.url.startsWith('/') ? page.url.substring(1) : page.url;
+
+    if (url === '') {
+        return currentUrl === '';
+    }
+    return currentUrl.startsWith(url);
+}
+
+</script>
+
+<template>
+    <aside class="">
+        <div class="mb-4">
+            <Link class="group flex items-center py-3" href="/">
+            <icon name="dashboard" class="mr-2 w-4 h-4"
+                :class="isUrl('') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
+            <div :class="isUrl('') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Dashboard</div>
+            </Link>
+        </div>
+        <div class="mb-4">
+            <Link class="group flex items-center py-3" href="/organizations">
+            <icon name="office" class="mr-2 w-4 h-4"
+                :class="isUrl('organizations') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
+            <div :class="isUrl('organizations') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Organizations
+            </div>
+            </Link>
+        </div>
+        <div class="mb-4">
+            <Link class="group flex items-center py-3" href="/contacts">
+            <icon name="users" class="mr-2 w-4 h-4"
+                :class="isUrl('contacts') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
+            <div :class="isUrl('contacts') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Contacts</div>
+            </Link>
+        </div>
+        <div class="mb-4">
+            <Link class="group flex items-center py-3" href="/reports">
+            <icon name="printer" class="mr-2 w-4 h-4"
+                :class="isUrl('reports') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
+            <div :class="isUrl('reports') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Reports</div>
+            </Link>
+        </div>
+    </aside>
+
+</template>
---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import tailwindcss from '@tailwindcss/vite'; // Importación necesaria
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        
+    ],
+    server: {
+        host: 'localhost',
+        hmr: {
+            host: 'localhost'
+        }
+    }
+});
