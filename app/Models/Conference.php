d031b88e569ffcc9d4a7d749e2d6a8066bfb2bd4
Jocelyn-AW <jocelyn@awsoftware.mx>
2026-03-06 17:05:15 -0600

    feat: add conferences crud

--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -3,8 +3,126 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Http\Helpers\Constants;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Conference extends Model
+class Conference extends Model implements HasMedia
 {
-    //
+    use InteractsWithMedia;
+
+    protected $table = Constants::TABLE_CONFERENCES;
+
+    protected $fillable = [
+        'name',
+        'main_topic',
+        'description',
+        'format',
+        'held_by',
+        'address',
+        'google_coords',
+        'link',
+        'organized_by',
+        'member_price',
+        'resident_price',
+        'guest_price',
+        'nurse_price',
+        'surgeon_price',
+        'is_active',
+        'bank_detail_id',
+        'additional_comments',
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $appends = [
+        'cover_url',
+        'gallery_urls',
+        'sponsors_logos_urls',
+        'program_url',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+        'bank_detail_id' => 'integer',
+        'resident_price' => 'decimal:2',
+        'guest_price' => 'decimal:2',
+        'member_price' => 'decimal:2',
+        'nurse_price' => 'decimal:2',
+        'surgeon_price' => 'decimal:2',
+    ];
+
+    //Morph Relations
+    public function speakers()
+    {
+        return $this->morphMany(EventSpeaker::class, 'event');
+    }
+    
+    public function sessions()
+    {
+        return $this->morphMany(EventSession::class, 'sessionable');
+    }
+
+    //Media
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('conference_covers')
+            ->acceptsMimeTypes(['image/jpeg', 'image/png', 'image/webp'])
+            ->useDisk('public')
+            ->singleFile()
+            ->withResponsiveImages();
+
+        $this->addMediaCollection('conference_gallery')
+            ->acceptsMimeTypes(['image/jpeg', 'image/png', 'image/webp'])
+            ->useDisk('public')
+            ->withResponsiveImages();
+
+        $this->addMediaCollection('conference_sponsor_logos')
+            ->acceptsMimeTypes(['image/jpeg', 'image/png', 'image/webp'])
+            ->useDisk('public')
+            ->withResponsiveImages();
+
+        $this->addMediaCollection('conference_program')
+            ->acceptsMimeTypes(['application/pdf'])
+            ->useDisk('public');
+    }
+
+    //Atributos
+    protected function coverUrl(): Attribute
+    {
+        return Attribute::make(function () {
+            return $this->getFirstMediaUrl('conference_covers');
+        });
+    }
+
+    protected function galleryUrls(): Attribute
+    {
+        return Attribute::make(function () {
+            return $this->getMedia('conference_gallery')->map(function ($media) {
+                return $media->getUrl();
+            });
+        });
+    }
+
+    protected function sponsorsLogosUrls(): Attribute
+    {
+        return Attribute::make(function () {
+            return $this->getMedia('conference_sponsor_logos')->map(function ($media) {
+                return $media->getUrl();
+            });
+        });
+    }
+
+    protected function programUrl(): Attribute
+    {
+        return Attribute::make(function () {
+            $media = $this->getFirstMedia('conference_program');
+            return $media ? $media->getUrl() : null;
+        });
+    }
 }
---
+++ database/migrations/2026_03_06_171844_add_nurse_ans_surgeon_prices_to_conferences.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('conferences', function (Blueprint $table) {
+            $table->dropColumn('date');
+            $table->dropColumn('specialties');
+        
+            $table->decimal('surgeon_price', 10, 2)
+                ->nullable()->default(null)
+                ->after('resident_price');
+            
+                $table->decimal('nurse_price', 10, 2)
+                ->nullable()->default(null)
+                ->after('surgeon_price');
+            
+            $table->foreignId('bank_detail_id')
+                ->nullable()                    
+                ->default(null)                 
+                ->after('is_active')
+                ->constrained('bank_details')
+                ->nullOnDelete();
+
+            $table->text('description')->nullable()->default(null)->after('main_topic');
+            $table->string('link')->nullable()->default(null)->after('google_coords');
+            $table->boolean('is_active')->default(true)->after('nurse_price');
+            $table->string('google_coords')->nullable()->default(null)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('conferences', function (Blueprint $table) {
+            $table->datetime('date')->nullable()->default(null);
+            $table->string('specialties')->nullable()->default(null);
+            $table->dropColumn('surgeon_price');
+            $table->dropColumn('nurse_price');
+            $table->dropColumn('link');
+            $table->dropColumn('is_active');
+            $table->dropColumn('description');
+            $table->dropForeign(['bank_detail_id']);
+            $table->dropColumn('bank_detail_id');
+        });
+    }
+};
