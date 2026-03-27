e04fda3ade9fe349ca1c20624019182af4560456
Jocelyn-AW <jocelyn@awsoftware.mx>
2026-03-27 12:16:03 -0600

    feat: add real data to dashboard, and change redirects

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\DirectoryController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Web\HistoryController;
@@ -17,9 +18,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [AuthenticatedSessionController::class, 'dashboard'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile',          [ProfileController::class, 'edit'])->name('profile.edit');
