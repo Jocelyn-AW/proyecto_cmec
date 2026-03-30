d3b4968ac4abcbdf35128a031ae79c1be55c83d8
Jocelyn-AW <jocelyn@awsoftware.mx>
2026-03-30 12:15:52 -0600

    feat: add placeholders in member directory

--- resources/js/Pages/Directory/Member/Index.vue
+++ resources/js/Pages/Directory/Member/Index.vue
@@ -283,28 +283,28 @@ watch(selectedState, (value, old) => {
                             <!-- Hospital -->
                             <div>
                                 <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">Hospital</label>
-                                <input type="text" v-model="clinic.hospital_name" maxlength="190"
+                                <input type="text" v-model="clinic.hospital_name" maxlength="190" placeholder="Ej: Hospital General No. 16"
                                     class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800" />
                                 <span v-if="errors[`clinics.${index}.hospital_name`]" class="text-red-500 text-xs flex justify-end mt-1">{{ errors[`clinics.${index}.hospital_name`] }}</span>
                             </div>
                             <!-- Direccion -->
                             <div class="col-span-2">
                                 <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">Dirección</label>
-                                <input type="text" v-model="clinic.address" maxlength="190"
+                                <input type="text" v-model="clinic.address" maxlength="190" placeholder="Ej: Av. Cuauhtémoc 123, Col. Centro, CDMX"
                                     class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800" />
                                 <span v-if="errors[`clinics.${index}.address`]" class="text-red-500 text-xs flex justify-end mt-1">{{ errors[`clinics.${index}.address`] }}</span>
                             </div>
                             <!-- Telefono -->
                             <div class="col-start-1">
                                 <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">Teléfono de contacto</label>
-                                <input type="text" v-model="clinic.phone" maxlength="12"
+                                <input type="text" v-model="clinic.phone" maxlength="12" placeholder="Ej: 55 125-123-00"
                                     class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800" />
                                 <span v-if="errors[`clinics.${index}.phone`]" class="text-red-500 text-xs flex justify-end mt-1">{{ errors[`clinics.${index}.phone`] }}</span>
                             </div>
                             <!-- Horario -->
                             <div class="col-span-2">
                                 <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">Horario</label>
-                                <input type="text" v-model="clinic.schedule" maxlength="190"
+                                <input type="text" v-model="clinic.schedule" maxlength="190" placeholder="Ej: Lunes a viernes de 9:00 a 18:00"
                                     class="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800" />
                                 <span v-if="errors[`clinics.${index}.schedule`]" class="text-red-500 text-xs flex justify-end mt-1">{{ errors[`clinics.${index}.schedule`] }}</span>
                             </div>
